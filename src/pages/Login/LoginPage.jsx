0332b83be76c2fc59221cff7e51e0548ff2463df
Alanis Yates <yatesalanis@gmail.com>
2023-02-21 22:00:19 -0500

    login basic

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,10 +1,23 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("email");
+  const [password, setPassword] = useState("pass");
+
+  const data = { email, password };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(data);
+    alert(data);
+  };
+
   return (
     <div>
-      <Button variant="contained">Login</Button>
+      <Button variant="contained" onClick={handleLogin}>
+        Login
+      </Button>
     </div>
   );
 }
