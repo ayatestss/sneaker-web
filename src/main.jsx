168e6beee41129dce8b0b43402686ba688bfe0de
Alanis Yates <yatesalanis@gmail.com>
2023-02-23 21:06:45 -0500

    add in apollo workin

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import { AuthProvider } from "./context/authContext";
 import { ThemeProvider } from "@mui/material/styles";
 import { appTheme } from "./theme/theme";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <ThemeProvider theme={appTheme}>
-          <App />
-        </ThemeProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <AuthProvider>
+          <ThemeProvider theme={appTheme}>
+            <App />
+          </ThemeProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
