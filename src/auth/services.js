8c917c12612b0e62d75733f1f7d0ff635bc51742
AlanisYates <58268342+AlanisYates@users.noreply.github.com>
2023-07-05 18:52:36 -0400

    Revert "Revert "Twitter-features""

--- src/auth/services.js
+++ src/auth/services.js
@@ -4,6 +4,11 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 import { FirebaseAuth } from "./firebase";
+import {
+  signInWithEmailAndPassword as signInWithEmail,
+  sendPasswordResetEmail as firebaseSendPasswordResetEmail,
+  createUserWithEmailAndPassword as firebaseCreateUser,
+} from "firebase/auth";
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -19,6 +24,8 @@ export const singInWithGoogle = async () => {
   }
 };
 
+// for login
+
 export const onAuthStateHasChanged = (setSession) => {
   onAuthStateChanged(FirebaseAuth, (user) => {
     if (!user) return setSession({ status: "no-authenticated", userId: null });
@@ -27,4 +34,30 @@ export const onAuthStateHasChanged = (setSession) => {
   });
 };
 
+// password reset
+export const sendPasswordResetEmail = async (email) => {
+  try {
+    await firebaseSendPasswordResetEmail(FirebaseAuth, email);
+    return true;
+  } catch (error) {
+    console.error("Error sending password reset email:", error);
+    return false;
+  }
+};
+
+// register page
+export const createUserWithEmailAndPassword = async (email, password) => {
+  try {
+    console.log("Creating user with email:", email);
+    const result = await firebaseCreateUser(FirebaseAuth, email, password);
+    console.log("User created successfully:", result.user.uid);
+    return result.user.uid;
+  } catch (error) {
+    console.error("Error creating user:", error);
+    throw error;
+  }
+};
+
 export const logOut = async () => await FirebaseAuth.signOut();
+
+export const signInWithEmailAndPassword = signInWithEmail;
