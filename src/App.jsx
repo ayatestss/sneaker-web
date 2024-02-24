5184af164048b54980f8a70b301edcabd1b4539d
Richard Colachagua <richardcolachagua@gmail.com>
2024-02-24 14:19:57 -0500

    "Frontend UI for contract status widget"

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { useContext } from "react";
@@ -10,6 +11,8 @@ import SignupPage from "./pages/SignupPage/SignupPage";
 import ErrorPage from "./pages/ErrorPage";
 import StripeWidget from "./components/StripeWidget/StripeWidget";
 =======
+=======
+>>>>>>> 73636de ("Frontend UI for contract status widget")
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { useContext } from 'react';
@@ -29,15 +32,46 @@ import { setContext } from '@apollo/client/link/context';
 >>>>>>> c23c81e (Update auth (#46))
 =======
 import { Dashboard } from './pages/Dashboard/Dashboard';
+<<<<<<< HEAD
 >>>>>>> ad6dd25 (add layout (#48))
+=======
+=======
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { useContext } from "react";
+import { AuthContext } from "./context/authContext";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useMode, ColorModeContext } from "./theme/theme";
+import HomePage from "./pages/HomePage/HomePage";
+import SignupPage from "./pages/SignupPage/SignupPage";
+import ErrorPage from "./pages/ErrorPage";
+import ContractStatusWidget from "./components/Contract-Status-Widget";
+>>>>>>> 93fbbd1 ("Frontend UI for contract status widget")
+>>>>>>> 73636de ("Frontend UI for contract status widget")
 
 function App() {
   const { status } = useContext(AuthContext);
   const [theme, colorMode] = useMode();
 
+<<<<<<< HEAD
   const httpLink = createHttpLink({
     uri: 'http://localhost:4000/graphql',
   });
+=======
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <div className="content-container">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route
+                path="/contract-status"
+                element={<ContractStatusWidget />}
+              />
+>>>>>>> 93fbbd1 ("Frontend UI for contract status widget")
 
 <<<<<<< HEAD
               <Route path="/stripewidget" element={<StripeWidget />} />
