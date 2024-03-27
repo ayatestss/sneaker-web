9b08f73e50731cab0de8339f6d852dd04d3bf04a
Richard Colachagua <89026636+richardcolachagua@users.noreply.github.com>
2024-03-27 19:52:53 -0400

    Update ContractStatus.js

--- src/components/ContractStatusWidget/ContractStatus.js
+++ src/components/ContractStatusWidget/ContractStatus.js
@@ -1,11 +1,1 @@
-import { gql } from "@apollo/client";
 
-export const GET_CONTRACT_STATUS_COUNTS = gql`
-  query GetContractStatusCounts {
-    contractStatusCounts {
-      notStarted
-      inProgress
-      done
-    }
-  }
-`;
