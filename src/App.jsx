ea98dab949dafe8709978c83f16b58a872ea4d67
AlanisYates <58268342+AlanisYates@users.noreply.github.com>
2024-04-06 22:30:12 -0400

    Update auth (#46)
    * Add resolver
    
    * inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,66 @@ import { useMode, ColorModeContext } from "./theme/theme";
 import HomePage from "./pages/HomePage/HomePage";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import ErrorPage from "./pages/ErrorPage";
-import SignupMember from "./pages/SignUpMemberPage/SignUpMemberPage";
+import LoginPage from "./pages/Login/LoginPage";
+import { ProtectedRoute } from "./components/PrivateRoute";
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 function App() {
   const { status } = useContext(AuthContext);
   const [theme, colorMode] = useMode();
 
+  const httpLink = createHttpLink({
+    uri: "http://localhost:4000/graphql",
+  });
+
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem("authToken");
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="App">
-          <div className="content-container">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/signupmember" element={<SignupMember />} />
-              {/* Error Page Route */}
-              <Route path="*" element={<ErrorPage />} />
-            </Routes>
+    <ApolloProvider client={client}>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="App">
+            <div className="content-container">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+
+                {/* Protected Routes */}
+                <Route
+                  path="/signup"
+                  element={
+                    <ProtectedRoute status={status}>
+                      <SignupPage />
+                    </ProtectedRoute>
+                  }
+                />
+
+                {/* Error Page Route */}
+                <Route path="*" element={<ErrorPage />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </ApolloProvider>
   );
 }
 
