a5359c2c4133d6bd94887333df4026335ccdda63
AlanisYates <58268342+AlanisYates@users.noreply.github.com>
2024-04-06 22:30:12 -0400

    Update auth (#46)
    * Add resolver
    
    * inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { useContext } from "react";
@@ -8,29 +9,84 @@ import HomePage from "./pages/HomePage/HomePage";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import ErrorPage from "./pages/ErrorPage";
 import StripeWidget from "./components/StripeWidget/StripeWidget";
+=======
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { useContext } from 'react';
+import { AuthContext } from './context/authContext';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { useMode, ColorModeContext } from './theme/theme';
+import HomePage from './pages/HomePage/HomePage';
+import SignupPage from './pages/SignupPage/SignupPage';
+import ErrorPage from './pages/ErrorPage';
+import LoginPage from './pages/Login/LoginPage';
+import { ProtectedRoute } from './components/PrivateRoute';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+>>>>>>> c23c81e (Update auth (#46))
 
 function App() {
   const { status } = useContext(AuthContext);
   const [theme, colorMode] = useMode();
 
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="App">
-          <div className="content-container">
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/signup" element={<SignupPage />} />
+  const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/graphql',
+  });
 
+<<<<<<< HEAD
               <Route path="/stripewidget" element={<StripeWidget />} />
               {/* Error Page Route */}
               <Route path="*" element={<ErrorPage />} />
             </Routes>
+=======
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('authToken');
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="App">
+            <div className="content-container">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+
+                {/* Protected Routes */}
+                <Route
+                  path="/signup"
+                  element={
+                    <ProtectedRoute status={status}>
+                      <SignupPage />
+                    </ProtectedRoute>
+                  }
+                />
+
+                {/* Error Page Route */}
+                <Route path="*" element={<ErrorPage />} />
+              </Routes>
+            </div>
+>>>>>>> c23c81e (Update auth (#46))
           </div>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </ApolloProvider>
   );
 }
 
