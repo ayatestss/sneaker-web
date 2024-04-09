f6afb5555b337a116dbac50131e57e85cd0aec6a
Richard Colachagua <richardcolachagua@gmail.com>
2024-04-08 20:46:50 -0400

    "updated stripe widget UI"

--- src/components/StripeWidget/StripeWidget.jsx
+++ src/components/StripeWidget/StripeWidget.jsx
@@ -15,24 +15,28 @@ function StripeWidget() {
       sx={{
         border: "3px solid #ccc",
         borderRadius: "10px",
-        height: "250px",
-        width: "50%",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        position: "absolute",
+        position: "fixed",
         top: "50%",
         left: "50%",
         transform: "translate(-50%, -50%)",
+        width: "65%",
+        height: "35%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <Stack>
+      <Stack direction="column">
         <Box
           sx={{
             display: "flex",
             bgcolor: "green",
             borderRadius: "50px",
             justifyContent: "center",
+            alignItems: "center",
+            width: "150px",
+            height: "50px",
+            marginBottom: "15px",
           }}
         >
           <Typography sx={{ mt: 1, fontSize: 30 }}>
@@ -46,12 +50,17 @@ function StripeWidget() {
           ${dummyData.nextPayout.amount}
         </Typography>
       </Stack>
-      <Stack direction="column" marginLeft={25}>
+      <Stack direction="column" marginLeft={15}>
         <Button
           variant="outlined"
           sx={{
             mb: 2,
             color: "white",
+            borderColor: "white",
+            border: "2px solid #ccc",
+            borderRadius: "10px",
+            width: "120px",
+            height: "50px",
           }}
           onClick={() => console.log("Navigate to statements")}
         >
@@ -61,10 +70,26 @@ function StripeWidget() {
           variant="outlined"
           sx={{
             color: "white",
+            borderColor: "white",
+            border: "2px solid #ccc",
+            borderRadius: "10px",
+            width: "120px",
+            height: "50px",
+            position: "relative",
           }}
           onClick={() => console.log("Navigate to Stripe Dashboard")}
         >
           Stripe
+          <Box
+            sx={{
+              width: "15px",
+              height: "15px",
+              bgcolor: "red",
+              borderRadius: "50%",
+              position: "absolute",
+              left: "15px",
+            }}
+          />
         </Button>
       </Stack>
     </Box>
