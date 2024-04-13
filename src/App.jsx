91a0dde7dfaae4b8d252159193886ea53724e91b
AlanisYates <58268342+AlanisYates@users.noreply.github.com>
2024-04-13 07:59:35 -0400

    add new route for stripe onboarding (#45)
    * add new route
    
    * test

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ function App() {
               <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
-
+                <Route path="stripeOnboarding" element={<StripeSignupPage />} />
                 {/* Protected Routes */}
                 <Route
                   path="/signup"
