de9ed50e4474f9b1369fc8b673807bf27a7cc72c
AlanisYates <58268342+AlanisYates@users.noreply.github.com>
2024-04-13 10:23:42 -0400

    add layout (#48)

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,11 @@ import { ProtectedRoute } from './components/PrivateRoute';
 import { ApolloProvider } from '@apollo/client';
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+<<<<<<< HEAD
 >>>>>>> 78daf92 (add new route for stripe onboarding (#45))
+=======
+import { Dashboard } from './pages/Dashboard/Dashboard';
+>>>>>>> ad6dd25 (add layout (#48))
 
 function App() {
   const { status } = useContext(AuthContext);
@@ -55,6 +59,7 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="stripeOnboarding" element={<StripeSignupPage />} />
+                <Route path="/dashboard" element={<Dashboard />} />
                 {/* Protected Routes */}
                 <Route
                   path="/signup"
