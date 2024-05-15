71665a7b7a7fe643ad3d7484aeb2b925c9738269
Richard Colachagua <richardcolachagua@gmail.com>
2024-05-14 22:12:41 -0400

    "Validation Schema, Yup implemented. UI Updates. handleClick functions created"

--- src/pages/Signup-Member/Signup-Member.jsx
+++ src/pages/Signup-Member/Signup-Member.jsx
@@ -3,15 +3,8 @@ import Logo from "../../assets/ss-logo.svg";
 import GoogleIcon from "@mui/icons-material/Google";
 import { Formik, Form, useField } from "formik";
 import { useNavigate } from "react-router-dom";
-import {
-  Button,
-  Container,
-  Alert,
-  Grid,
-  TextField,
-  Stack,
-  Box,
-} from "@mui/material";
+import { Button, Alert, Grid, TextField, Stack, Box } from "@mui/material";
+import * as Yup from "yup";
 
 const FormikTextField = ({ name, ...props }) => {
   const [field, meta] = useField(name);
@@ -44,120 +37,120 @@ const SignupMember = () => {
     }
   };
 
+  const handleGoogleSignUp = async () => {
+    try {
+      const firebaseId = await signUpWithGoogle();
+      navigate("/signup-info");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
   return (
-    <Container maxWidth="md">
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        padding: "20px",
+      }}
+    >
+      <Stack
+        alignItems="center"
+        spacing={2}
+        sx={{ width: "100%", maxWidth: "360px", px: 2 }}
       >
         <Box
+          component="img"
+          src={Logo}
+          alt="Logo"
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-            bgcolor: "black",
-            padding: "20px",
+            width: "80%",
+            maxWidth: "300px",
+            height: "auto",
+            my: 4,
+            "@media (min-width:600px)": {
+              maxWidth: "80%",
+            },
           }}
+        />
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
         >
-          <Stack
-            alignItems="center"
-            spacing={2}
-            sx={{ width: "100%", maxWidth: "360px", px: 2 }}
-          >
-            <Box
-              component="img"
-              src={Logo}
-              alt="Logo"
-              sx={{
-                width: "80%",
-                maxWidth: "300px",
-                height: "auto",
-                my: 4,
-                "@media (min-width:600px)": {
-                  maxWidth: "80%",
-                },
-              }}
-            />
-            <Formik
-              initialValues={{
-                email: "",
-                password: "",
-              }}
-              onSubmit={handleSubmit}
-              validate={(values) => {
-                const errors = {};
-                if (!values.email) {
-                  errors.email = "Email is required";
-                }
-
-                if (!values.password) {
-                  errors.password = "Password is required";
-                }
-
-                return errors;
-              }}
-            >
-              {() => (
-                <Form>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                      <FormikTextField
-                        name="email"
-                        label="Email"
-                        variant="outlined"
-                        fullWidth
-                      />
-                      <FormikTextField
-                        name="password"
-                        label="Password"
-                        variant="outlined"
-                        fullWidth
-                        sx={{
-                          mt: 2,
-                          py: 1,
-                        }}
-                      />
-                    </Grid>
-                  </Grid>
-                  <Stack>
-                    <Button
-                      type="submit"
-                      variant="contained"
-                      aria-label="Sign Up"
+          {({ isSubmitting, values }) => {
+            return (
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <FormikTextField
+                      name="email"
+                      label="Email"
+                      variant="outlined"
+                      fullWidth
+                    />
+                    <FormikTextField
+                      name="password"
+                      label="Password"
+                      variant="outlined"
+                      fullWidth
                       sx={{
                         mt: 2,
                         py: 1,
                       }}
-                    >
-                      SIGN UP
-                    </Button>
-                    <Button
-                      variant="contained"
-                      aria-label="Sign Up With Google"
-                      startIcon={<GoogleIcon />}
-                      sx={{
-                        mt: 2,
-                      }}
-                    >
-                      SIGN UP WITH GOOGLE
-                    </Button>
-                  </Stack>
-                </Form>
-              )}
-            </Formik>
-            {error && (
-              <Alert severity="error" color="error">
-                {error}
-              </Alert>
-            )}
-          </Stack>
-        </Box>
-      </div>
-    </Container>
+                    />
+                  </Grid>
+                </Grid>
+                <Stack>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    aria-label="Sign Up"
+                    sx={{
+                      mt: 2,
+                      py: 1,
+                    }}
+                    onClick={handleSubmit}
+                  >
+                    SIGN UP
+                  </Button>
+                  <Button
+                    variant="contained"
+                    aria-label="Sign Up With Google"
+                    startIcon={<GoogleIcon />}
+                    sx={{
+                      mt: 2,
+                    }}
+                  >
+                    onClick={handleGoogleSignUp}
+                    SIGN UP WITH GOOGLE
+                  </Button>
+                </Stack>
+              </Form>
+            );
+          }}
+        </Formik>
+
+        {error && (
+          <Alert severity="error" color="error">
+            {error}
+          </Alert>
+        )}
+      </Stack>
+    </Box>
   );
 };
 
