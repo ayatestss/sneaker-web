3f6d4b3ada53aaf64ee13c28eeb7ea655b0059cd
Richard Colachagua <richardcolachagua@gmail.com>
2024-06-19 20:53:57 -0400

    "removed widget files"

--- src/App.jsx
+++ src/App.jsx
@@ -6,76 +6,29 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { useMode, ColorModeContext } from "./theme/theme";
 import HomePage from "./pages/HomePage/HomePage";
 import SignupPage from "./pages/SignupPage/SignupPage";
-// import LoginPage from "./pages/Login/LoginPage";
 import ErrorPage from "./pages/ErrorPage";
-// import ContractStatusWidget from "./components/ContractStatusWidget/ContractStatusWidget";
-import StripeWidget from "./components/StripeWidget/StripeWidget";
-import SignupMember from "./pages/Signup-Member/Signup-Member";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { ProtectedRoute } from "./components/PrivateRoute";
 
 function App() {
   const { status } = useContext(AuthContext);
   const [theme, colorMode] = useMode();
 
-  const httpLink = createHttpLink({
-    uri: "http://localhost:4000/graphql",
-  });
-
-  const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    const token = localStorage.getItem("authToken");
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    };
-  });
-
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-
   return (
-    <ApolloProvider client={client}>
-      <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <div className="App">
-            <div className="content-container">
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                {/* <Route path="/login" element={<LoginPage />} /> */}
-                <Route
-                  path="/signup"
-                  element={
-                    <ProtectedRoute status={status}>
-                      <SignupPage />
-                    </ProtectedRoute>
-                  }
-                />
-                {/* <Route
-                  path="/contract-status"
-                  element={<ContractStatusWidget />}
-                /> */}
-                <Route path="/stripewidget" element={<StripeWidget />} />
-                <Route path="*" element={<ErrorPage />} />
-                <Route path="/SignupMember" element={<SignupMember />} />
-              </Routes>
-            </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <div className="content-container">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/signup" element={<SignupPage />} />
+
+              {/* Error Page Route */}
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
           </div>
-        </ThemeProvider>
-      </ColorModeContext.Provider>
-    </ApolloProvider>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
