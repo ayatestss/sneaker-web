cf34796b00357aae1146ff8066384fda3572175f
Alanis Yates <yatesalanis@gmail.com>
2024-08-27 15:56:20 -0700

    update pages

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,17 @@
-import React, { useState, useRef } from 'react';
-import { Box, Stack, Typography, Divider, useMediaQuery } from '@mui/material';
-import Header from './Header';
-import Footer from './Footer';
-import { TypeAnimation } from 'react-type-animation';
-import StyledButton from './StackedButton';
-import FeaturesSection from './FeaturesSection';
-import ScrollToNextIcon from './ScrollToNextIcon';
-import PricingCard from './PricingCard';
-import PricingTable from './PricingTable';
-import { useNavigate } from 'react-router-dom';
-import ContactSection from './ContactSection';
-import ContractWidget from './ContractWidget';
+import React, { useState, useRef } from "react";
+import { Box, Stack, Typography, Divider, useMediaQuery } from "@mui/material";
+import Header from "./Header";
+import Footer from "./Footer";
+import { TypeAnimation } from "react-type-animation";
+import StyledButton from "./StackedButton";
+import FeaturesSection from "./FeaturesSection";
+import ScrollToNextIcon from "./ScrollToNextIcon";
+import PricingTable from "./PricingTable";
+import { useNavigate } from "react-router-dom";
+import ContactSection from "./ContactSection";
 
 function HomePage() {
-  const isSmallScreen = useMediaQuery('(max-width:600px)');
+  const isSmallScreen = useMediaQuery("(max-width:600px)");
 
   const featuresSectionRef = useRef(null); // Create a ref for the FeaturesSection
   const pricingSectionRef = useRef(null);
@@ -22,14 +20,14 @@ function HomePage() {
   const scrollToRef = (ref) => {
     window.scrollTo({
       top: ref.current.offsetTop - (isSmallScreen ? 120 : 100),
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
   const navigate = useNavigate();
 
   const handleSignupClick = () => {
-    navigate('/signup');
+    navigate("/signup");
   };
 
   return (
@@ -43,41 +41,41 @@ function HomePage() {
 
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
         <Box
           sx={{
-            height: '100vh',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textAlign: 'center',
-            flexDirection: 'column',
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+            flexDirection: "column",
           }}
         >
           <Typography
             variant="h2"
             component="h1"
             sx={{
-              fontWeight: '700',
-              fontSize: { lg: '96px', md: '4rem', sm: '3rem', xs: '3rem' },
-              marginTop: { xs: '20px', md: '0' },
-              marginBottom: '32px',
-              wordWrap: 'break-word',
+              fontWeight: "700",
+              fontSize: { lg: "96px", md: "4rem", sm: "3rem", xs: "3rem" },
+              marginTop: { xs: "20px", md: "0" },
+              marginBottom: "32px",
+              wordWrap: "break-word",
               lineHeight: { xs: 1.2, sm: 1.3 },
             }}
           >
-            <Box sx={{ height: '100px' }}>
+            <Box sx={{ height: "100px" }}>
               <TypeAnimation
                 sequence={[
-                  'Elevate your Collection',
+                  "Elevate your Collection",
                   1000,
-                  'Streamline your Process',
+                  "Streamline your Process",
                   1000,
-                  'Build your Brand',
+                  "Build your Brand",
                   1000,
                 ]}
                 speed={50}
@@ -89,10 +87,10 @@ function HomePage() {
           <Typography
             variant="h6"
             sx={{
-              fontWeight: '400',
-              fontSize: { xs: '16px', md: '1.25rem' },
-              maxWidth: '800px',
-              marginBottom: '48px',
+              fontWeight: "400",
+              fontSize: { xs: "16px", md: "1.25rem" },
+              maxWidth: "800px",
+              marginBottom: "48px",
             }}
           >
             Your all-in-one solution: Where restoration experts elevate their
@@ -115,12 +113,12 @@ function HomePage() {
 
         <Box
           sx={{
-            height: isSmallScreen ? '100%' : '100vh',
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
+            height: isSmallScreen ? "100%" : "100vh",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
           }}
           ref={featuresSectionRef}
         >
@@ -128,22 +126,22 @@ function HomePage() {
         </Box>
         <Box
           sx={{
-            height: '100vh',
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textAlign: 'center',
+            height: "100vh",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
           }}
         >
-          <Box sx={{ width: '100%', height: '100%', paddingTop: '10%' }}>
+          <Box sx={{ width: "100%", height: "100%", paddingTop: "10%" }}>
             <Typography
               fontWeight="bold"
               gutterBottom
               ref={pricingSectionRef}
               sx={{
-                fontSize: { xs: '3rem', sm: '5.2rem' },
+                fontSize: { xs: "3rem", sm: "5.2rem" },
                 lineHeight: 1.4,
               }}
             >
@@ -152,10 +150,10 @@ function HomePage() {
 
             <PricingTable
               features={[
-                'Unlimited Prospect Intakes',
-                'Direct Messaging',
-                'Direct Stripe Payments',
-                'Business analytics',
+                "Unlimited Prospect Intakes",
+                "Direct Messaging",
+                "Direct Stripe Payments",
+                "Business analytics",
               ]}
               onButtonClick={handleSignupClick}
             />
@@ -166,19 +164,16 @@ function HomePage() {
         </Box>
         <Box
           sx={{
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            paddingTop: '12%'
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            paddingTop: "12%",
           }}
           ref={contactSectionRef}
         >
           <ContactSection />
         </Box>
-      </Box>
-      <Box mt={5}>
-        <ContractWidget />
       </Box>
       <Footer />
     </Box>
