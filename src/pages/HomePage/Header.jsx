8d5c0bbd121964a8ba4623ce29c0078d2e438cc1
Richard Colachagua <richardcolachagua@gmail.com>
2024-10-14 20:21:24 -0400

    "login button created"

--- src/pages/HomePage/Header.jsx
+++ src/pages/HomePage/Header.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Hidden from '@mui/material/Hidden';
-import Tabs from '@mui/material/Tabs';
-import { Tab } from '@mui/material';
-import Sneakers from '../../../assets/sneakers-header.png';
-import StyledButton from './StackedButton';
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Hidden from "@mui/material/Hidden";
+import Tabs from "@mui/material/Tabs";
+import { Tab } from "@mui/material";
+import Sneakers from "../../../assets/sneakers-header.png";
+import StyledButton from "./StackedButton";
 
 const Header = ({ pricingRef, contactRef, featureRef, onButtonClick }) => {
   return (
     <>
-      <AppBar position="sticky" style={{ zIndex: 9999, background: 'black' }}>
+      <AppBar position="sticky" style={{ zIndex: 9999, background: "black" }}>
         <Toolbar
           style={{
-            minHeight: '120px',
-            display: 'flex',
-            justifyContent: 'space-between',
+            minHeight: "120px",
+            display: "flex",
+            justifyContent: "space-between",
           }}
         >
           <Hidden smdown>
@@ -27,13 +27,15 @@ const Header = ({ pricingRef, contactRef, featureRef, onButtonClick }) => {
             />
           </Hidden>
           <Hidden mdDown>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
               <Tabs textColor="inherit">
                 <Tab label="Features" onClick={featureRef} />
                 <Tab label="Pricing" onClick={pricingRef} />
                 <Tab label="Contact" onClick={contactRef} />
               </Tabs>
-
+              <StyledButton style={{}} onClick={onButtonClick}>
+                Login
+              </StyledButton>
               <StyledButton onClick={onButtonClick}>Sign Up</StyledButton>
             </div>
           </Hidden>
