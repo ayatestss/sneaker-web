86574ad98d67fc136a055d57a3093bf028f01f75
Gerardo Delao <Gerardo_delao@ymail.com>
2024-12-16 17:12:08 -0700

    added router

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,15 @@
 import { Box, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import StyledButton from "./HomePage/StackedButton";
 import Logo from "../../assets/SNEAKER SOCIETY (Transparency).png";
 
 export default function ErrorPage() {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
+
   return (
     <Box
       sx={{
@@ -14,39 +21,36 @@ export default function ErrorPage() {
         textAlign: "center",
       }}
     >
-      {/* Logo */}
       <Box
         component="img"
         src={Logo}
         alt="Logo"
         sx={{
-          width: "500px", // Default width for larger screens
-          height: "auto", // Auto height to maintain aspect ratio
+          width: "500px",
+          height: "auto",
           display: "block",
           margin: 0,
           padding: "0",
           "@media (max-width:600px)": {
-            width: "300px", // Adjusted width for mobile
+            width: "300px",
           },
         }}
       />
 
-      {/* Typography */}
       <Typography
         sx={{
-          fontSize: "24px", // Default font size for larger screens
+          fontSize: "24px",
           marginBottom: "24px",
           fontWeight: "500",
           "@media (max-width:600px)": {
-            fontSize: "22px", // Adjusted font size for mobile
+            fontSize: "22px",
           },
         }}
       >
         Looks like this page does not exist!
       </Typography>
 
-      {/* Button */}
-      <StyledButton>Go Home</StyledButton>
+      <StyledButton onClick={handleGoHome}>Go Home</StyledButton>
     </Box>
   );
 }
